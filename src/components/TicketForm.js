810ce647c98749535eb072b956168ef458a7afcf
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2025-04-27 15:51:00 -0500

    Se crearon los primeros campos de formulario

---
+++ src/components/TicketForm.js
@@ -0,0 +1,78 @@
+import { clear } from "@testing-library/user-event/dist/clear";
+import React, { useState } from "react";
+
+export default function TicketForm() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState("1");
+
+  const priorityLabels = {
+    1: "Baja",
+    2: "Media",
+    3: "Alta",
+  };
+
+  const clearForm = () => {
+    setTitle("");
+    setDescription("");
+    setPriority("1");
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const ticketData = {
+      id: new Date().toISOString(),
+      title,
+      description,
+      priority,
+    };
+
+    clearForm();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="ticket-form">
+      <div>
+        <label>Titulo</label>
+        <input
+          type="text"
+          value={title}
+          className="form-input"
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        ></input>
+      </div>
+      <div>
+        <label>Description</label>
+        <textArea
+          type="text"
+          value={description}
+          className="form-input"
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        ></textArea>
+      </div>
+
+      <fieldset className="priority-fieldset">
+        <legend>Priority</legend>
+        {Object.entries(priorityLabels).map(([value, label]) => (
+          <label key={value} className="priority-label">
+            <input
+              type="radio"
+              value={value}
+              checked={priority === value}
+              className="priority-input"
+              onChange={(e) => setPriority(e.target.value)}
+            ></input>
+            {label}
+          </label>
+        ))}
+      </fieldset>
+
+      <button type="submit" className="btn btn-primary">
+        Enviar ticket
+      </button>
+    </form>
+  );
+}
